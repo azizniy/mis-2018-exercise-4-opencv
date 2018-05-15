b48181d523803406d793651af3b41b3cfc0f57a3
azizniy <azizniy@mail.ru>
2018-05-15 22:21:40 +0200

    red nose with respect to the face position

--- app/src/main/java/com/example/mis/opencv/MainActivity.java
+++ app/src/main/java/com/example/mis/opencv/MainActivity.java
@@ -1,26 +1,28 @@
 package com.example.mis.opencv;
 
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.view.WindowManager;
-import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -34,6 +36,9 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     private CameraBridgeViewBase mOpenCvCameraView;
     private boolean              mIsJavaCamera = true;
     private MenuItem             mItemSwitchCamera = null;
+    private CascadeClassifier cascadeClassifier;
+    private int absoluteFaceSize;
+    String cascade;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -43,6 +48,9 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
                 {
                     Log.i(TAG, "OpenCV loaded successfully");
                     mOpenCvCameraView.enableView();
+                    cascade  = initAssetFile("haarcascade_frontalface_default.xml");
+                    cascadeClassifier =  new CascadeClassifier(cascade);
+
                 } break;
                 default:
                 {
@@ -70,6 +78,15 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+
+
+
+            //cascadeClassifier.load(cascade);
+
+
+
+        //Log.d(TAG, "path = " + path);
     }
 
     @Override
@@ -100,6 +117,7 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
     }
 
     public void onCameraViewStarted(int width, int height) {
+        absoluteFaceSize = (int) (height * 0.2);
     }
 
     public void onCameraViewStopped() {
@@ -107,13 +125,6 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 
-        //return inputFrame.rgba();
-        /*
-        Mat col  = inputFrame.rgba();
-        Rect foo = new Rect(new Point(100,100), new Point(200,200));
-        Imgproc.rectangle(col, foo.tl(), foo.br(), new Scalar(0, 0, 255), 3);
-        return col;
-        */
 
         Mat gray = inputFrame.gray();
         Mat col  = inputFrame.rgba();
@@ -122,6 +133,35 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
         Imgproc.Canny(gray, tmp, 80, 100);
         Imgproc.cvtColor(tmp, col, Imgproc.COLOR_GRAY2RGBA, 4);
 
+        
+        //https://www.mirkosertic.de/blog/2013/07/realtime-face-detection-on-android-using-opencv/
+        MatOfRect faces = new MatOfRect();
+
+        if (cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(gray, faces, 1.1, 2, 2,
+                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+        }
+        // Draw rectangle on face
+        Rect[] facesArray = faces.toArray();
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        for (int i = 0; i <facesArray.length; i++){
+            //Imgproc.rectangle(col, facesArray[i].tl(), facesArray[i].br(), new Scalar(255, 0, 0, 255), 3);
+            //Log.d(TAG, "faces: " + facesArray[i].tl());
+
+            //nose position almost in a center of a rectangle
+            Point c = new Point(((facesArray[i].br().x + facesArray[i].tl().x))/2,
+                                ((facesArray[i].br().y + facesArray[i].tl().y))/1.9);
+
+            //the radius of circle changes according to the proximity of face
+            int r = 20/(displayMetrics.heightPixels/facesArray[i].width);
+            //Log.d(TAG, "faces: " + r);
+
+            Imgproc.circle(col,c,r, new Scalar(255,0,0), 30);
+
+            }
         return col;
     }
 
@@ -132,8 +172,15 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
             InputStream is = getAssets().open(filename);
             OutputStream os = new FileOutputStream(file);
             byte[] data = new byte[is.available()];
-            is.read(data); os.write(data); is.close(); os.close();
-        } catch (IOException e) { e.printStackTrace(); }
+            is.read(data);
+            os.write(data);
+            is.close();
+            os.close();
+            Log.d(TAG,"SUCCESS: ");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.d(TAG,"Error: "+e);
+        }
         Log.d(TAG,"prepared local file: "+filename);
         return file.getAbsolutePath();
     }
